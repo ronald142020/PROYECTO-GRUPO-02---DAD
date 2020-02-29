27527dd3dbd452aa060448ca5d6ff7942004f37f
ronald142020 <61603809+ronald142020@users.noreply.github.com>
2020-02-29 09:18:29 -0500

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,137 @@
+<?php
+  $page_title = 'Admin página de inicio';
+  require_once('includes/load.php');
+  // Checkin What level user has permission to view this page
+   page_require_level(1);
+?>
+<?php
+ $c_categorie     = count_by_id('categories');
+ $c_product       = count_by_id('products');
+ $c_provider      = count_by_id('providers');
+ $c_user          = count_by_id('users');
+ $recent_products = find_recent_product_added('5');
+ $recent_providers = find_recent_provider_added('5');
+ ?>
+<?php include_once('layouts/header.php'); ?>
+
+<div class="row">
+   <div class="col-md-6">
+     <?php echo display_msg($msg); ?>
+   </div>
+</div>
+  <div class="row">
+    <div class="col-md-3">
+       <div class="panel panel-box clearfix">
+         <div class="panel-icon pull-left bg-green">
+          <i class="glyphicon glyphicon-user"></i>
+        </div>
+        <div class="panel-value pull-right">
+          <h2 class="margin-top"> <?php  echo $c_user['total']; ?> </h2>
+          <p class="text-muted">Usuarios</p>
+        </div>
+       </div>
+    </div>
+    <div class="col-md-3">
+       <div class="panel panel-box clearfix">
+         <div class="panel-icon pull-left bg-red">
+          <i class="glyphicon glyphicon-list"></i>
+        </div>
+        <div class="panel-value pull-right">
+          <h2 class="margin-top"> <?php  echo $c_categorie['total']; ?> </h2>
+          <p class="text-muted">Categorías</p>
+        </div>
+       </div>
+    </div>
+    <div class="col-md-3">
+       <div class="panel panel-box clearfix">
+         <div class="panel-icon pull-left bg-blue">
+          <i class="glyphicon glyphicon-shopping-cart"></i>
+        </div>
+        <div class="panel-value pull-right">
+          <h2 class="margin-top"> <?php  echo $c_product['total']; ?> </h2>
+          <p class="text-muted">Productos</p>
+        </div>
+       </div>
+    </div>
+    
+    <div class="col-md-3">
+       <div class="panel panel-box clearfix">
+         <div class="panel-icon pull-left bg-green">
+          <i class="glyphicon glyphicon-list-alt"></i>
+        </div>
+        <div class="panel-value pull-right">
+          <h2 class="margin-top"> <?php  echo $c_provider['total']; ?> </h2>
+          <p class="text-muted">Proveedores</p>
+        </div>
+       </div>
+    </div>
+</div>
+
+  <div class="col-md-4">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <strong>
+          <span class="glyphicon glyphicon-th"></span>
+          <span>Proveedores recientemente añadidos</span>
+        </strong>
+      </div>
+      <div class="panel-body">
+
+        <div class="list-group">
+            <?php foreach ($recent_providers as  $recent_provider): ?>
+            <a class="list-group-item clearfix" href="">
+                <h5 class="list-group-item-heading">
+                 <span class="">
+                 <?php echo $recent_provider['nombre']; ?>
+                  </span>
+                 <span class="label label-success pull-right">
+                 RUC: <?php echo $recent_provider['ruc']; ?>
+                  </span>
+                </h5>
+            </a>
+      <?php endforeach; ?>
+    </div>
+  </div>
+ </div>
+</div>
+
+
+ <div class="col-md-4">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <strong>
+          <span class="glyphicon glyphicon-th"></span>
+          <span>Productos recientemente añadidos</span>
+        </strong>
+      </div>
+      <div class="panel-body">
+
+        <div class="list-group">
+      <?php foreach ($recent_products as  $recent_product): ?>
+            <a class="list-group-item clearfix" href="edit_product.php?id=<?php echo    (int)$recent_product['id'];?>">
+                <h4 class="list-group-item-heading">
+                 <?php if($recent_product['media_id'] === '0'): ?>
+                    <img class="img-avatar img-circle" src="uploads/products/no_image.jpg" alt="">
+                  <?php else: ?>
+                  <img class="img-avatar img-circle" src="uploads/products/<?php echo $recent_product['image'];?>" alt="" />
+                <?php endif;?>
+                <?php echo remove_junk(first_character($recent_product['name']));?>
+                  <span class="label label-warning pull-right">
+                 S/ <?php echo (int)$recent_product['sale_price']; ?>
+                  </span>
+                </h4>
+                <span class="list-group-item-text pull-right">
+                <?php echo remove_junk(first_character($recent_product['categorie'])); ?>
+              </span>
+          </a>
+      <?php endforeach; ?>
+    </div>
+  </div>
+ </div>
+</div>
+
+
+
+ </div>
+  
+<?php include_once('layouts/footer.php'); ?>
